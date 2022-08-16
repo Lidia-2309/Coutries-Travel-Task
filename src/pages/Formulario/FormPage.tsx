ca4beb3957c89699edf546412917858d0b2491f0
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2022-08-16 11:25:57 -0300

    2

---
+++ src/pages/Formulario/FormPage.tsx
@@ -0,0 +1,14 @@
+import './PageForm.css'
+
+export const FormPage = () => {
+    return (
+        <div className="newproject_container">
+            <h1>Formulário</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>Formulario</p>
+        </div>
+    );
+
+}
+
+export default FormPage
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { Home } from '../pages';
+import { Home } from '../pages/Home';
+import { FormPage } from '../pages/Formulario';
 
 export const Routes = () => {
     return(
         <BrowserRouter>
         <Switch>
-            <Route path='/'>
+            <Route exact path='/home'>
                 <Home/>
             </Route>
+            <Route exact path='/form'>
+                <FormPage/>
+            </Route>
             <Route path='/'>
-                <Redirect to='/'/>
+                <Redirect to='/home'/>
             </Route>
         </Switch>
     </BrowserRouter>
