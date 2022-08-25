c95caf29b7675e388b17115c68606120c97f8eb4
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2022-08-25 12:47:27 -0300

    Pagina de naçoes atualizada

--- src/pages/Formulario/FormPage.tsx
+++ src/pages/Formulario/FormPage.tsx
@@ -5,6 +5,9 @@ import CountrySelect from '../../components/graphql/Continents';
 import { Link } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
 
+
+//____________________________________________________________
+
 // initialize a GraphQL client
 const client = new ApolloClient({
     cache: new InMemoryCache(),
@@ -21,10 +24,7 @@ const LIST_CONTINENTS = gql`
  
 `;
 
-
-
-
-
+//________________________________________________//
 
 
 interface Formulario {
@@ -34,7 +34,12 @@ interface Formulario {
     CPF: string;
 }
 
+
+
 export const FormPage = () => {
+
+    const { data } = useQuery(LIST_CONTINENTS, { client });
+    const [show, setShow] = useState(true);
 
     //USESTATE
 
@@ -57,120 +62,110 @@ export const FormPage = () => {
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="exampleInputName" 
-                    className="form-label">Nome</label>
-                    <input type="text" 
-                    className="form-control" 
-                    id="name" 
-                    required
-                    value={formState.name}
-                    onChange={(event) =>
-                        setFormState({
-                            ...formState,
-                            name: event.currentTarget?.value || "teste",
-                        })}
+                    <label htmlFor="exampleInputName"
+                        className="form-label">Nome</label>
+                    <input type="text"
+                        className="form-control"
+                        id="name"
+                        required
+                        value={formState.name}
+                        onChange={(event) =>
+                            setFormState({
+                                ...formState,
+                                name: event.currentTarget?.value || "teste",
+                            })}
                     />
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" 
-                    className="form-label">E-mail</label>
-                    <input type="email" 
-                    className="form-control" 
-                    id="email" 
-                    aria-describedby="emailHelp"
-                    required
-                    value={formState.email}
-                    onChange={(event) =>
-                        setFormState({
-                            ...formState,
-                            email: event.currentTarget?.value || "",
-                        })}
+                    <label htmlFor="exampleInputEmail1"
+                        className="form-label">E-mail</label>
+                    <input type="email"
+                        className="form-control"
+                        id="email"
+                        aria-describedby="emailHelp"
+                        required
+                        value={formState.email}
+                        onChange={(event) =>
+                            setFormState({
+                                ...formState,
+                                email: event.currentTarget?.value || "",
+                            })}
                     />
                 </div>
-                
+
                 <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" 
-                    className="form-label">Senha</label>
-                    <input type="password" 
-                    className="form-control" 
-                    id="password"
-                    required
-                    value={formState.password}
-                    onChange={(event) =>
-                        setFormState({
-                            ...formState,
-                            password: event.currentTarget?.value || "",
-                        })}
+                    <label htmlFor="exampleInputPassword1"
+                        className="form-label">Senha</label>
+                    <input type="password"
+                        className="form-control"
+                        id="password"
+                        required
+                        value={formState.password}
+                        onChange={(event) =>
+                            setFormState({
+                                ...formState,
+                                password: event.currentTarget?.value || "",
+                            })}
                     />
-                    
+
                 </div>
-                
+
                 <div className="CPF">
-                <label htmlFor="CPF" 
-                    className="form-label">CPF</label>
+                    <label htmlFor="CPF"
+                        className="form-label">CPF</label>
                     <InputMask
-                        className='form-control' 
+                        className='form-control'
                         mask="999.999.999-99"
                         id="CPF"
                         required
                         value={formState.CPF}
                         onChange={(event) =>
-                        setFormState({
-                            ...formState,
-                            CPF: event.currentTarget?.value || "",
-                        })}
-                    />     
+                            setFormState({
+                                ...formState,
+                                CPF: event.currentTarget?.value || "",
+                            })}
+                    />
                 </div>
 
                 <div className="select">
-                    <CountrySelect/>
+                    <CountrySelect />
                 </div>
-                
+
                 <button
 
                     className="btn btn-primary"
-                    onClick={()=>console.log(formState)}>
-                    
+                    onClick={() => console.log(formState)}>
+
                     Enviar
-                
+
                 </button>
 
-                
-                 
+                {/* <div className='nations'>
+                    {show && <p>
+                        {data.countries.map(countries => (
+                            <option key={countries.name} value={countries.name}>
+                                {countries.name}
+                                {countries.code}
+                            </option>
+                        ))}
+                    </p>
+                    }
+                    <button
+                        type="button"
+                        className="button"
+                        onClick={() => setShow(!show)}>
+                        Paises
+                    </button>
+                </div> */}
+
+
             </form>
-            <button
-
-                    className="btn btn-primary"
-                    onClick={()=>function Select () {
-                        const { data, loading, error } = useQuery(LIST_CONTINENTS, { client });
-                    
-                        if (loading || error) {
-                            return <p>{error ? error.message : 'Loading...'}</p>;
-                        }
-                    
-                        return (
-                            <div className='nations'> 
-                                <p >
-                                    {data.countries.map(countries => (
-                                        <option key={countries.name} value={countries.name}>
-                                            {countries.name}
-                                            {countries.code}
-                                        </option>
-                                    ))}
-                                </p>
-                            </div>      
-                        );
-                    }}>
-                    
-                    Paises
-                
-                </button>
 
         </div>
-        
+
     );
-   
+
 }
 
 //console.log(formState)
--- src/pages/Lista_Paises/nacoes.tsx
+++ src/pages/Lista_Paises/nacoes.tsx
@@ -20,31 +20,32 @@ const LIST_CONTINENTS = gql`
 // create a component that renders a select input for coutries
 export const Select = () => {
     const { data, loading, error } = useQuery(LIST_CONTINENTS, { client });
+    const [ show, setShow ] = useState(true);
 
     if (loading || error) {
         return <p>{error ? error.message : 'Loading...'}</p>;
     }
 
     return (
-        <button
-                    className="button"
-                    onClick={()=>
-                            <div className='nations'> 
-                                <p >
-                                    {data.countries.map(countries => (
-                                        <option key={countries.name} value={countries.name}>
-                                            {countries.name}
-                                            {countries.code}
-                                        </option>
-                                    ))}
-                                </p>
-                            </div>
-                        }>
-                    
-                    Paises
-
-                </button>
-    );
+        <div className='nations'> 
+            { show && <p>
+                {data.countries.map(countries => (
+                    <option key={countries.name} value={countries.name}>
+                        {countries.name}
+                        {countries.code}
+                   </option>
+                  ))}
+              </p>
+            }
+          <button   
+                type="button" 
+                className="button" 
+                onClick={()=>setShow(!show)}>  
+                Paises        
+          </button>
+      </div> 
+                     
+    )
 }
 
 export default Select;
