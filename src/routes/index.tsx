df2e486bb9c49fd92d75f946d95bb4c0db2472e4
Ana Lidia Lisboa de Andrade <analidialisboadeand@gmail.com>
2022-09-06 11:57:34 -0300

    Adicionando prototipo de uma tabela simples

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { Home } from '../pages/Home';
 import { FormPage } from '../pages/Formulario';
 import { Select } from '../pages/Lista_Paises/nacoes';
 import PAISESSELECAO from '../pages/Teste/teste';
-
+import BasicTable from '../components/table/BasicTable';
 
 
 export const Rotas = () => {
@@ -15,6 +15,7 @@ export const Rotas = () => {
                 <Route path='/form' element={<FormPage />} />
                 <Route path='/paises' element={<Select />} />
                 <Route path='/teste' element={<PAISESSELECAO />} />
+                <Route path='/table' element={<BasicTable/>} />
             </Routes>
         </BrowserRouter>
     );
